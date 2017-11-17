5eb64494825060283a1dca51549c5bf811b1cd5e
Laurent Louf <laurent.louf@iot-makers.com>
2017-11-17 15:28:53 +0100

    Change the algorithm to get RGB values from HSB values to an algorithm with floating point precision.

--- src/color.cpp
+++ src/color.cpp
@@ -2,51 +2,65 @@
 
 void HSBToRGB( unsigned int i_hue, unsigned int i_saturation, unsigned int i_brightness, unsigned int *o_red, unsigned int *o_green, unsigned int *o_blue )
 {
-    if (i_saturation == 0)
-    {
-        // achromatic (grey)
-        *o_red = *o_green = *o_blue = i_brightness;
-    }
-    else
-    {
-        unsigned int scaledHue = (i_hue * 6);
-        unsigned int sector = scaledHue >> 8; // sector 0 to 5 around the color wheel
-        unsigned int offsetInSector = scaledHue - (sector << 8);	// position within the sector
-        unsigned int p = (i_brightness * ( 255 - i_saturation )) >> 8;
-        unsigned int q = (i_brightness * ( 255 - ((i_saturation * offsetInSector) >> 8) )) >> 8;
-        unsigned int t = (i_brightness * ( 255 - ((i_saturation * ( 255 - offsetInSector )) >> 8) )) >> 8;
+  if( i_saturation == 0 )
+  {
+    *o_red    = i_brightness ;
+    *o_green  = i_brightness ;
+    *o_blue   = i_brightness ;
+  }
+  else
+  {
+    float hh, p, q, t, ff;
+    long  sector ;
+    float saturation = i_saturation / 255.0, brightness = i_brightness / 255.0 ;
 
-        switch( sector ) {
-        case 0:
-            *o_red = i_brightness;
-            *o_green = t;
-            *o_blue = p;
-            break;
-        case 1:
-            *o_red = q;
-            *o_green = i_brightness;
-            *o_blue = p;
-            break;
-        case 2:
-            *o_red = p;
-            *o_green = i_brightness;
-            *o_blue = t;
-            break;
-        case 3:
-            *o_red = p;
-            *o_green = q;
-            *o_blue = i_brightness;
-            break;
-        case 4:
-            *o_red = t;
-            *o_green = p;
-            *o_blue = i_brightness;
-            break;
-        default:    // case 5:
-            *o_red = i_brightness;
-            *o_green = p;
-            *o_blue = q;
-            break;
-        }
+    hh = i_hue;
+    hh /= 60.0;
+    sector = (long)hh;
+
+    ff = hh - sector;
+    p = brightness  * (1.0 -  saturation);
+    q = brightness  * (1.0 - ( saturation * ff));
+    t = brightness  * (1.0 - ( saturation * (1.0 - ff)));
+
+    switch(sector)
+    {
+      case 0:
+        *o_red = brightness * 255 ;
+        *o_green = t * 255;
+        *o_blue = p * 255;
+      break;
+
+      case 1:
+        *o_red = q * 255;
+        *o_green = brightness * 255 ;
+        *o_blue = p * 255;
+      break;
+
+      case 2:
+        *o_red = p * 255;
+        *o_green = brightness * 255 ;
+        *o_blue = t * 255;
+      break;
+
+      case 3:
+        *o_red = p * 255;
+        *o_green = q * 255;
+        *o_blue = brightness * 255 ;
+      break;
+
+      case 4:
+        *o_red = t * 255;
+        *o_green = p * 255;
+        *o_blue = brightness * 255 ;
+      break;
+
+      case 5:
+      default:
+        *o_red = brightness * 255 ;
+        *o_green = p * 255;
+        *o_blue = q * 255;
+      break;
     }
+  }
 }
